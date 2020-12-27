203a7de0258a0d973c2fe6029011542a7a128494
Anon Praditrungwattana <Anon17925@gmail.com>
2020-12-27 22:58:28 +0700

    create app first

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Antd from "ant-design-vue";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import "ant-design-vue/dist/antd.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(Antd);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
---
+++ src/views/Admin/AdminHome.vue
@@ -0,0 +1,49 @@
+<template>
+  <a-layout id="components-layout-demo-top-side-2">
+    <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' ,background: '#fff'}" >
+      <v-row no-gutters>
+        
+      </v-row>
+    </a-layout-header>
+    <a-layout-sider width="230" :style="{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0, background: '#fff', marginTop:'70px' }">
+      <a-menu
+       style="width: 256px"
+       :default-selected-keys="['1']"
+       :default-open-keys="['sub1']"
+       :mode="mode"
+       :theme="theme"
+       v-for="item in ListNavbar"
+       :key="item.key"
+      >
+        <a-menu-item :key="item.key">
+          <a-icon :type="item.icon" />{{item.name}}
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+  </a-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ListNavbar: [
+        { key: 'sub1',
+          icon: 'bank',
+          name: 'บริษัทจัดซื้อจัดจ้าง',
+          path: ''
+        },
+        { key: 'sub2',
+          icon: 'shop',
+          name: 'ร้านค้าของฉัน',
+          path: ''
+        },
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
